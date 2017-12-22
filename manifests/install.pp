047ee9b12be95b563905ba91744e2e28fefa6ec3
Simon Kurka <dev@simon.kurka.cc>
2017-12-22 18:00:06 +0100

    qualify paths

--- manifests/install.pp
+++ manifests/install.pp
@@ -33,32 +33,32 @@ class yanic::install inherits yanic {
     content => 'puppet:///modules/yanic/yanic.service'
   } ->
   exec { "yanic mkdir webserver_webroot":
-    command => "mkdir -p ${webserver_webroot}",
-    unless  => "test -d ${webserver_webroot}",
+    command => "/bin/mkdir -p ${webserver_webroot}",
+    unless  => "/usr/bin/test -d ${webserver_webroot}",
   } ->
   exec { "yanic mkdir dir_nodes_state_path":
-    command => "mkdir -p ${dir_nodes_state_path}",
-    unless => "test -d ${dir_nodes_state_path}",
+    command => "/bin/mkdir -p ${dir_nodes_state_path}",
+    unless => "/usr/bin/test -d ${dir_nodes_state_path}",
   } ->
   exec { "yanic mkdir dir_nodes_output_meshviewer_ffrgb_path":
-    command => "mkdir -p ${dir_nodes_output_meshviewer_ffrgb_path}",
-    unless => "test -d ${dir_nodes_output_meshviewer_ffrgb_path}",
+    command => "/bin/mkdir -p ${dir_nodes_output_meshviewer_ffrgb_path}",
+    unless => "/usr/bin/test -d ${dir_nodes_output_meshviewer_ffrgb_path}",
   } ->
   exec { "yanic mkdir dir_nodes_output_meshviewer_nodes_path":
-    command => "mkdir -p ${dir_nodes_output_meshviewer_nodes_path}",
-    unless => "test -d ${dir_nodes_output_meshviewer_nodes_path}",
+    command => "/bin/mkdir -p ${dir_nodes_output_meshviewer_nodes_path}",
+    unless => "/usr/bin/test -d ${dir_nodes_output_meshviewer_nodes_path}",
   } ->
   exec { "yanic mkdir dir_nodes_output_meshviewer_graph_path":
-    command => "mkdir -p ${dir_nodes_output_meshviewer_graph_path}",
-    unless => "test -d ${dir_nodes_output_meshviewer_graph_path}",
+    command => "/bin/mkdir -p ${dir_nodes_output_meshviewer_graph_path}",
+    unless => "/usr/bin/test -d ${dir_nodes_output_meshviewer_graph_path}",
   } ->
   exec { "yanic mkdir dir_nodes_output_nodelist_path":
-    command => "mkdir -p ${dir_nodes_output_nodelist_path}",
-    unless => "test -d ${dir_nodes_output_nodelist_path}",
+    command => "/bin/mkdir -p ${dir_nodes_output_nodelist_path}",
+    unless => "/usr/bin/test -d ${dir_nodes_output_nodelist_path}",
   } ->
   exec { "yanic mkdir dir_database_connection_logging_path":
-    command => "mkdir -p ${dir_database_connection_logging_path}",
-    unless => "test -d ${dir_database_connection_logging_path}",
+    command => "/bin/mkdir -p ${dir_database_connection_logging_path}",
+    unless => "/usr/bin/test -d ${dir_database_connection_logging_path}",
   }
 
 }
